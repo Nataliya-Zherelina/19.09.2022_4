1ca4d95be788c9e37175468434cfeeab4a165641
Nataliya <kutus6a@mail.ru>
2022-09-19 14:46:01 +0300

    Ввод чисел

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
